63248d1ee1f3099ff5d2f0b5799b073746f55d31
Mhockman2 <62911938+Mhockman2@users.noreply.github.com>
2020-06-15 14:22:33 -0700

    Add files via upload

--- Gaussian.py
+++ Gaussian.py
@@ -3,30 +3,35 @@
 # Michael Hockman
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Setting up the variables, assuming we are starting at midnight the previous night
-variance = 1
+variance = 2
 mean = 12  # peak temp at noon
 t_end = 24  # Assuming we start at midnight and end at midnight
-size = 100  # How many source terms we are going to get
-t = np.linspace(0, 24, size)
-A = 75
+size = 24  # How many source terms we are going to get, for testing purposes
+t = np.linspace(0, 24, size)  # time starting at hour 0 and ending at hour 24
+A = 75  # variable for the Gaussian, controls max value
+B = 0  # variable for the Gaussian, controls the offset
 
-
-# takes in a time ti, variance v, mean m, and amplitude A
-# returns the output of a Gaussian function with mean m and variance v
-def sourceTerms(ti, A, m, v):
-    coefficient = A / (np.sqrt(2 * np.pi) * v)
-    exponential = -(ti - m) ** 2 / (2 * v ** 2)
-    return coefficient * np.exp(exponential)
+# takes in a time ti
+# returns the output of a Gaussian function with mean and variance depending on the variables defined above
+def sourceTerms(ti):
+    coefficient = A / (np.sqrt(2 * np.pi) * variance)
+    exponential = -(ti - mean) ** 2 / (2 * variance ** 2)
+    return B + coefficient * np.exp(exponential)
 
 
 terms = []  # Source terms
 segments = []  # Time intervals each source term is mapped to, in 24 hour clock time
 for i in range(0, size):
     segments.append([[24 / size * i], [24 / size * (i + 1)]])
-    terms.append(sourceTerms(i, A, mean, variance))
+    terms.append(sourceTerms(i))
 
+# print(segments)
+# print(terms)
 
-print(segments)
-print(terms)
+plt.xlabel("time (hrs)")
+plt.ylabel("temperature (c)")
+plt.plot(t, terms)
+plt.show()
---
+++ heatPolar Gaussian Test.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 20:03:51 2020
+
+ref: https://scicomp.stackexchange.com/questions/20279/how-can-i-solve-wave-equation-for-circular-membrane-in-polar-coordinates
+
+visualization ref with mayavi: https://docs.enthought.com/mayavi/mayavi/installation.html
+
+This code solves equation (10) in Potter and Andresen
+
+To Fix:
+    some issues with visualization
+    add source term
+    justify boundary and initial conditions
+    Assume rho, c, k are all constants
+    Check CFL condition
+    the role of jn_zero and jv
+    vectorize to increase efficiency (can wait till later)
+
+@author: yajun
+"""
+import numpy as np
+from scipy.special import jv, jn_zeros
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from Gaussian import sourceTerms
+
+# from mayavi import mlab
+
+# %% Parameters; play with parameters, and see if warnings would go away
+Nr = 50
+N_phi = 50
+N_steps = 8000
+radius = 0.09
+c = 2.7148 * 10 ** (-7)  # = k/(rho*c) in equation (2) of Potter and Anredsen paper #changed to match potter anderson
+dphi = 2 * np.pi / N_phi
+dphi2 = dphi * dphi
+dr = radius / Nr
+dr2 = dr * dr
+dt = 0.001
+if (1 - 4 * dt * c / dr2 - 4 * dt * c / (dr2 * dphi2)) ** 2 < 1:  # CFL condition.
+    # The maximum value for dt is dr*dphi/(2*c)
+    print("CFL condition is satisfied")
+else:
+    print("CFL condition not satisfied, shrink dt by 0.0001")
+    print(str((1 - 4 * dt * c / dr2 - 4 * dt * c / (dr2 * dphi2)) ** 2) + "> 1")
+    dt = dt * 0.0001
+# %% Initial conditions; waiting for data
+r = np.linspace(0, radius, Nr)
+phi = np.linspace(0, 2 * np.pi, N_phi)
+R, Phi = np.meshgrid(r, phi)
+X = R * np.cos(Phi)
+Y = R * np.sin(Phi)
+Z = np.cos(Phi) * (R - R ** 3)
+print("initial condition sin(phi)*(r-r^3)")
+# T has to be float, can not make it int
+T = np.zeros((N_steps, Nr, N_phi))
+T[0, :, :] = Z.T
+# %% visualize initial condition in 3D
+
+
+# %% Stepping
+k1 = c * dt / dr2
+for t in range(1, N_steps):
+    for i in range(0, Nr - 1):
+        ri = max(r[i], 0.5 * dr)  # To avoid the singularity at r=0
+        k2 = c * dt / (2 * ri * dr)
+        k3 = c * dt / (dphi2 * ri ** 2)
+        k4 = c / (0.46 * dr)  # Changed to match potter anderson
+        # En = 0 #uncomment this to make source terms always 0.
+        En = i == (Nr - 2)
+        #        print(k2,k3,i)
+        for j in range(0, N_phi - 1):
+            T[t, i, j] = T[t - 1, i, j] \
+                         + k1 * (T[t - 1, i + 1, j] - 2 * T[t - 1, i, j] + T[t - 1, i - 1, j]) \
+                         + k2 * (T[t - 1, i + 1, j] - T[t - 1, i - 1, j]) \
+                         + k3 * (T[t - 1, i, j + 1] - 2 * T[t - 1, i, j] + T[t - 1, i, j - 1]) \
+                         - k4 * sourceTerms(dphi * j)  # Need to use sourceTerms function from sourceTerms.py
+
+        T[t, :, -1] = T[t, :, 0]  # Update the values for phi=2*pi, BC in phi
+    #        T[t,0,i]?? T[t,R,i]??
+    print(np.max(abs(T[t, :, :])), t)
+# print(T[1,:,:])
+# %% visualization with matplolib BUGGY, NEED TO FIX
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+#
+# u,v = np.meshgrid(X.T, Y.T)
+# surf = ax.plot_surface(u, v, 10*T[999], cmap = cm.coolwarm, linewidth = 0, antialiased = False)
+# fig.colorbar(surf, shrink = 0.5, aspect = 5)
+# plt.show()
+
+"""   
+boundary condition for theta:    T[t, i, -1] = T[t, i, 0]
+boundary condition for radius direction:    T[t, @R, phi] 
+T[t, @center,] (assume) = 21.6 (assume it be some small varying function)
+
+
+ """
+
+# %% visualization with mayavi
+# surf = mlab.mesh(X.T, Y.T, 10*T[999], colormap='RdYlBu')
+# mlab.show()
