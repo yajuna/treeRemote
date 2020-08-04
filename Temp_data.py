6db31a18855443c963300b3161d407591c42b6cc
Mhockman2 <62911938+Mhockman2@users.noreply.github.com>
2020-08-03 17:40:20 -0700

    Add files via upload

--- Temp_data.py
+++ Temp_data.py
@@ -9,7 +9,8 @@ Takes the temperature data and creates a function out of it
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = [59, 58, 57, 56, 55, 55, 55, 56, 57, 59, 62, 64, 67, 69, 71, 73, 74, 75, 75, 74, 70, 66, 63, 62, 60]
+T = np.array([59, 58, 57, 56, 55, 55, 55, 56, 57, 59, 62, 64, 67, 69, 71, 73, 74, 75, 75, 74, 70, 66, 63, 62, 60])
+T = (T-32)*5/9+273.15
 #  print(np.size(T))
 
 #  Defines each point on the x-axis in which we have a corresponding data point from T
@@ -38,20 +39,32 @@ def temp(m, t0):
     #  rounded because you can't have a decimal index
     i = round(t0 / z)
     #  print("i = " + str(i))
-
+    """
     #  Plot the full temperature data
     plt.plot(x[:], y[:])
     plt.title('1 day temperature, Tacoma WA')
     plt.xlabel('Hour')
     plt.ylabel('Temperature (F)')
     plt.xlim(0, 24)
-    plt.ylim(40, 80)
+    plt.ylim(280, 300)
     plt.show()
+    """
 
     #  Return the temperature data from the correct index and convert to Kelvin
-    return (y[i]-32)*5/9+273.15
+    return y[i]
 
 
 #  Check to see if it gives good results
 Z = temp(1000, 1)
 print(Z)
+
+
+def tempArray(t1):
+    x = np.linspace(0, 24, np.size(t1)+1)
+
+    #  Creates the linear interpolation of the data
+    y = np.interp(x, t, T)
+    print("size y = " + str(np.size(y)))
+
+    print("size range = " + str(np.arange(1, np.size(t1))))
+    return y[np.arange(1, np.size(t1)+1)]
