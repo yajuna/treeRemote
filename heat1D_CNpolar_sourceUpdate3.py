f39e43a1c37182ede9c1789db26fc4671bcf9e91
yajuna <yajun.an@gmail.com>
2020-08-19 13:43:49 -0700

    fix typo at def of alpha and gamma. typos were mismatched index, and division by dt instead of dr

--- heat1D_CNpolar_sourceUpdate3.py
+++ heat1D_CNpolar_sourceUpdate3.py
@@ -4,7 +4,7 @@
 Created on Tuesday 07 14 14:55:12 2020
 1D code for heat equation in polar, with Crank-Nicolson
 Ref: LeVeque Chapter 9, pg 183
-Handling of source term: a primer on PDEs, Salsa et al.
+Handling of source term: a primer on PDEs, Salsa et al. pg. 99
 u_t = a*(1/r*u_r + u_rr)
 a = rho*c/k
 u(x,0)=eta(x) initial condition
@@ -46,14 +46,14 @@ beta = dt / (a * dr ** 2)
 
 alpha = np.ones(m - 1)
 for j in range(alpha.size):
-    alpha[j] = dt / (4 * a * r[j + 1] * dt) - 0.5 * beta
-# neumann condition at outer boundary
-alpha[-1] = -beta
+    alpha[j] = dt / (4 * a * r[j + 1] * dr) - 0.5 * beta
+# use following for neumann condition at outer boundary
+# alpha[-1] = -beta
 
 
 gamma = np.ones(m - 1)
 for j in range(1, gamma.size):
-    gamma[j] = dt / (4 * a * r[j + 1] * dt) + 0.5 * beta
+    gamma[j] = dt / (4 * a * r[j] * dr) + 0.5 * beta
 #    print(r[j+1])
 gamma[0] = beta  # define inner neumann bdry condition 1st row
 
@@ -96,7 +96,7 @@ soln = []
 U0 = eta(m)
 
 # %%
-#  Solving Ax1 = Bx0 + b, this is B. No flux results in different last row from before
+#  Solving Ax1 = Bx0 + b, this is B. If no flux, then different last row from before
 
 B = sparse.diags([-alpha, np.ones(m) - beta, gamma], [-1, 0, 1], shape=(m, m)).toarray()
 
@@ -108,9 +108,11 @@ soln.append(U0)
 
 for i in range(n - 1):
     rhs = B.dot(U0)
-    # dirichlet bdry condition with g1, source at bdry with gs
-    # rhs[-1] = rhs[-1] + (dt / (4 * a * r[-1] * dr) + 0.5 * beta) * (g1(i) + g1(i + 1))
-    rhs[-1] = rhs[-1] + 2* dr *(dt / (4 * a * r[-1] * dr) + 0.5 * beta) * (g1(i) + g1(i + 1)) + dt/(2*a*dr*k)*(gs(i) + gs(i + 1))
+    # dirichlet bdry condition with g1, source at bdry with gs; average gs at time i and i + 1
+    rhs[-1] = rhs[-1] + (dt / (4 * a * r[-1] * dr) + 0.5 * beta) * (g1(i) + g1(i + 1)) + dt/(2*a*dr*k)*(gs(i) + gs(i + 1))
+
+# if neumann condition for g1(t)
+    # rhs[-1] = rhs[-1] + 2* dr *(dt / (4 * a * r[-1] * dr) + 0.5 * beta) * (g1(i) + g1(i + 1)) + dt/(2*a*dr*k)*(gs(i) + gs(i + 1))
     U1 = np.linalg.solve(tridiag, rhs)  # sparse.linalg.lsqr(tridiag, rhs)
     U0 = U1
     soln.append(U0)
@@ -126,7 +128,7 @@ print("max and min of soln at final step = ", np.max(soln_plot[-1, :]), np.min(s
 grid_point = 29
 plt.plot(t, soln_plot[:, grid_point], '.r-')
 plt.title('Temperature with combined source term at grid point r=%i ' % grid_point)
-plt.axis([0,12,0,1200])
+plt.axis([0,12,273,300])
 plt.xlabel('Time since 7:00am (hrs)')
-plt.ylabel('Temperature Distribution')
+plt.ylabel('Temperature Distribution (K)')
 plt.show()
