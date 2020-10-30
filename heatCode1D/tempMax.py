b8a9d536ded63fc4fdc235b77d36246f61fb4277
yajuna <yajuna@uw.edu>
2020-10-30 11:40:31 -0700

    modifies tempMax.py

--- heatCode1D/tempMax.py
+++ heatCode1D/tempMax.py
@@ -6,7 +6,7 @@ Created on Fri Oct 23 12:04:09 2020
 This code searches for the time within 12 hours where the max of temp difference
 between center and bark of tree occurs
 
-Current has logic error, need to compare 
+Study time that gives max temp difference. Variable gridPoints.
 
 @author: yajun
 """
@@ -19,21 +19,22 @@ from sourceS import * # source term at bdry
 from Temp_dataVec import * # boundary at tree bark
 #%%
 config = dict()
-config['gridPoints'] = 50
+config['gridPoints'] = 750
 config['timeSteps'] = 1000
 mu,sigma = 1, 0.001 # mean and standard deviation
-config['thermalConductivity'] = 0.12*np.random.normal(mu, sigma, 6)
+config['thermalConductivity'] = 0.12*np.ones(12) #0.12*np.random.normal(mu, sigma, 6)
 config['heatCapacity_rhoc'] = 1.7
 config['at_point'] = 38
 config['time'] = np.linspace(0, 1000, 50, endpoint = False)
 
 import heat1dK as h1
 
+
 c = h1.temp(config)
 
-c.shape # (1000,50)
+print("shape of array ", c.shape) # (1000,gridPoint)
 
-tempDiff = np.zeros(c.shape[0])
+tempDiff = np.zeros(c[0].shape[0]) # number of rows, number of time steps
 
 for j in range(tempDiff.size):
     tempDiff[j] = np.abs(c[j,0] - c[j,-1])
@@ -43,7 +44,16 @@ MaxTempIndex = np.argmax(tempDiff)
 print("Max temperature difference occurs at", MaxTempIndex, "time step", ", the difference is", tempDiff[MaxTempIndex])   
     
 
+# loop for different gridPoints. Ran out of memory
+"""
+c = []
+for j in range(100):
+    config['gridPoints'] = config['gridPoints'] + 50
+    c.append(h1.temp(config))
+    
+print("shape of each array, and length of list", c[0].shape, len(c))    
 
+"""
 
 
 
