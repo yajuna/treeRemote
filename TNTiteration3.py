55c2f082a7c54af6d7c56bf50e747df3d1a2aea2
Mhockman2 <62911938+Mhockman2@users.noreply.github.com>
2021-01-13 16:36:49 -0800

    Add files via upload

--- TNTiteration3.py
+++ TNTiteration3.py
@@ -41,6 +41,12 @@ param = {"Ta": 29 + 273.2, "Tb": 22 + 273.2, "Va": 10, "qrads": 650, "Pr": 0.707
 
 # Function we need to solve for Tb
 def F(Tb):
+    C = 0.193
+    m = 0.618
+    Pr = 0.707
+    Ka = 26.3
+    nu = 15.89 * 10 ** -6
+    Va = 10
     epsilon = 0.8
     sigma = 5.67*10**-8
     Ta = 29+273.2
@@ -49,12 +55,14 @@ def F(Tb):
     Kt = 0.11
     DeltaT = 100/1000
     r1 = rb - 100 / 1000
+    Re = Va * (2 * rb) / nu
+    Nu = C * (Re ** m) * Pr ** (1 / 3)  # Nusselt number
+    h = Nu * Ka / (rb * 2)  # W/m2-k heat transfer coefficient
     Rcond = np.log(rb / r1) / (2 * np.pi * L * Kt)  # K/W
     qcond = DeltaT / Rcond  # W
     qrads = 650
     qeq = qcond-qrads
-    return qeq/(2*L*epsilon*pi*rb*sigma*Ta**3 + 2*L*epsilon*pi*rb*sigma*Ta**2*Tb + 2*L*epsilon*pi*rb*sigma*Ta*Tb**2 + 2*L*epsilon*pi*rb*sigma*Tb**3 + 2*L*hr*pi*rb) - Tb
-
+    return Ta + qeq/(2*np.pi*L*h*rb + 2*L*epsilon*rb*sigma*np.pi*(Ta**3 + Ta**2*Tb + Ta*Tb**2 + Tb**3))-Tb
 
 def outerBdry(**param):
     Ta = param["Ta"]
@@ -102,14 +110,14 @@ def outerBdry(**param):
     #     Tb = Tb2
     #     print("hr=", hr, "Rrad=", Rrad, "Req=", Req, "DeltaTeq=", DeltaTeq, "Tb=", Tb, "at step j =", j)
 
-    Tbfinal = scipy.optimize.root(F, Tb) - 273.2 # In Celcius
-    Tb = Tbfinal + 273.2 # need Tb in kelvin to calculate hr
+    Tb = scipy.optimize.broyden1(F, 300)
     hr = epsilon * sigma * (Tb + Ta) * (Tb ** 2 + Ta ** 2)
+    Tbfinal = Tb - 273.2
     print("hr=", hr, "Tb=", Tbfinal)
     return Tbfinal
 
 
-outerBdry()
+outerBdry(**param)
 
 """
 % Simple script to test the idea of iteration as a method to solve for
