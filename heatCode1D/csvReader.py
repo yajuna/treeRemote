43e2b30f7d764ce1c5f5a94839f9c47ccd04c955
yajuna <yajun.an@gmail.com>
2021-03-11 11:28:54 -0800

    add csvReader.py

--- heatCode1D/csvReader.py
+++ heatCode1D/csvReader.py
@@ -3,8 +3,10 @@
 """
 Created on Tue Feb 16 11:21:10 2021
 
-csv reader for TNT_weather data. 
+csv reader for TNT data. 
 Reads specific column, then convert to np array
+
+first portion is reference for format
 
 @author: yajuna
 """
@@ -23,6 +25,19 @@ temp1 = np.asarray([float(t) for t in temp])
 windspeed = data.windspeed[1:].tolist()
 windspeed1 = np.asarray([float(v) for v in windspeed])
 
+#%% Use data from Feb 16; Feb 15 data maybe incomplete
 
-# test value of center tree temperature
-Ta = np.random.uniform(low=21.5 + 273.2, high=25 + 273.2, size=(50,))
+colnames = ['time', 'temperature', 'windspeed', 'solar']
+
+dataWeather = pandas.read_csv('weatherJoaoPessoaFeb15-19.csv', names=colnames)
+
+time16 = dataWeather.time.tolist()
+
+temp16 = dataWeather.temperature[5:28].tolist() # row 6 ro 29
+temp16np = np.asarray([float(t) for t in temp16])
+
+windspeed16 = dataWeather.windspeed[5:28].tolist()
+windspeed16np = np.asarray([float(v) for v in windspeed16])
+
+solar16 = dataWeather.solar[5:28].tolist()
+solar16np = np.asarray([float(v) for v in solar16])
